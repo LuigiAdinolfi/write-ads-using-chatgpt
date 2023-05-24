4c2f71505b6db6a60711d40456fa63535aa62c0a
Luigi Adinolfi <luigi.adinolfi@students.fhnw.ch>
2023-05-24 20:08:41 +0200

    update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,11 @@ const container = document.getElementById("root");
 const root = createRoot(container!);
 
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <App />
-  </BrowserRouter>
+    <React.StrictMode>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
